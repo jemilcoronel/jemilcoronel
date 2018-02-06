3a7d8e2a38ee8e2e5f0303e488dc3ff1e1ba7ee1
Jemil Coronel <36122848+jemilcoronel@users.noreply.github.com>
2018-02-06 08:07:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SEILF INFORMATION</title>
+</head>
+<body bgcolor="#fffacc">
+<center><h1>MY FIRST PROJECT</h1></center>
+<h2>PERSONAL DATA</h2>
+<p><h4>Name:<u> Jemil P. Coronel</u></h4></p>
+<p><h4>Address:<u> Inayagan City of Naga, Cebu</u></h4></p>
+<p><h4>Contact:<u> 09325333901</u></h4></p>
+<p><h4>Email:<u> Jemil.coronel@gmail.com</u></h4></p>
+<p><h4>Age:<u> 22</u></h4></p>
+<p><h4>Gender:<u> Male</u></h4></p>
+<p><h4>D.O.B.:<U> May 25 1995</u></h4></p>
+<p><h4>Father's Name:<u> Efren B. Coronel</u></h4></p>
+<p><h4>Mother's Name:<u> Isabel R. Coronel</u></h4></p>
+<p><h4>Siblings:<u> 4 brothers & 2 sisters</u></h4></p>
+<h2>EDUCATIONAL BACKGROUND</h2>
+<p><h4>Elementary:<u> Inayagan Elementary School</u></h4></p>
+<p><h4>High School:<u> ASL/Naga National High School</u></h4></p>
+<p><h4>College:<u> CTU-Naga Extension Campus</u></h4></p>
+<center><h2>BIOGRAPHY</h2></center>
+ 
+<p><h4><br>I am Jemil P. Coronel from Gutalac Zamboanga del Norte but now presently lived in Inayagan city of Naga Cebu.
+I'm studying in CTU Naga Extension Canpus with the course of BSICT.
+ 
+<br>When I was 13 years old my father died. We are six siblings;two sisters and 4 brothers,I am the oldest. After the<
+death of father my uncle brought us here in Cebu. When we were in Cebu,I lived in my uncle's house and my one brother
+too. My sisters lived in my grandma's house,they continued their study there. Our mother cannot send us to school,
+provide food,and shelter, because she has a heart problem.<br>
+<br>My uncle Timothy where I lived in there house said to me;"continue your study in Elementary I will provide you".
+I continue to study in Elementary untill I gradute.After I graduted in Elementary my friends tempted me to use cigarettes,
+and drink lechor. At that time my life was damage because of my worldly friends.My uncle stoped to send me to school
+because of my wrong doings.Years go by I regret myself why I entered that kind of evil doing.There's a time that where
+we about to captured by the law because of our deeds. My mother always advice me to stop my evildoings,beceuse many times
+where we about to captured by the law,therefore I decided to stop my frailty. The fact that I stop my frailty because my 
+mother has a heart problem. I force myself to stop all that evildoings,and eventully I did it.After that I continue to 
+study in high school until I gradute.<br>
+<br>Eventhough,we were poor we always pray to the Lord that He will gave us good health. And now I still studying in 
+CTU-Naga Extension Campus. I have lot of learning from my past,therefore I move on and begin to face my future. I long to 
+finish my study.And may the Lord bless me.</p><h4>
+</body>
+</html>
